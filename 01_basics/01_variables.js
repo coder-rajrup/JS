40214831717a337849f9ad27d2a6ac0a30dacbc8
Rajrup Das <166900923+coder-rajrup@users.noreply.github.com>
2025-09-27 11:14:32 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =155553
+let accountEmail ="raj234example@gmail.com "
+var acountPassword = "12345"
+accountyCity= "jaipur"
+let accountState;
+
+// accountId = 2 //not allowed
+
+accountEmail ="hdgbd021233@.com"
+accountPassword = "54321"
+accountyCity = "Kolkata"
+
+console.log(accountId)
+console.table([accountEmail, accountId, accountPassword, accountyCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+ "use strict"; //treat all JS code as a new version
+ 
+
+ let name = "John"; //string
+ let age = 30; //number
+ let isStudent = true; //boolean
+
+
+ //number ==> 2 to 53
+ //bigint ==> larger than 53
+ //string ==> '',"",``
+ //boolean ==> true/false
+ //null ==> standalone value
+ //undefined ==> uninitialized value
+ //object ==> key-value pairs
+ //symbol ==> unique value
+
+    console.log(typeof null);
+    console.log(typeof age);
+    console.log(typeof isStudent);
