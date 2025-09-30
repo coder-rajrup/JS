85d01fbdeb2257dde0f379bf651d324abbd1ead9
Rajrup Das <166900923+coder-rajrup@users.noreply.github.com>
2025-09-30 09:59:20 +0000

    Strings

---
+++ 01_basics/06_String.js
@@ -0,0 +1,35 @@
+// const name = " Raj"
+// const repoCount = 50
+// console.log(` Hello ${name}. You have ${repoCount} repositories`);
+
+const gameName = new String('Chess_')
+
+// console.log(gameName[4]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(0, -3)
+console.log(anotherString);
+
+
+
+const newString1 = "     Rajput      "
+console.log(newString1);
+console.log(newString1.trim());
+
+const url = "https://www.youtube.com/lear.nwith.rajput"
+console.log(url.replace('learn', '-'));
+console.log(url.includes('baby'));
+console.log(url.split('.'));
+
+
+
