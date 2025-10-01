eb7fefcb5bc0b2016029ee9a42531866b6c1162f
Rajrup Das <166900923+coder-rajrup@users.noreply.github.com>
2025-10-01 16:27:58 +0000

    num math

---
+++ 01_basics/07_NumsandMaths.js
@@ -0,0 +1,66 @@
+const score = 400
+
+const balance = new Number(100)
+
+// console.log(score)
+// console.log(balance)
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.123456
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString());
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(Number.POSITIVE_INFINITY);
+// console.log(Number.NEGATIVE_INFINITY);
+// console.log(Number.NaN);
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//Math Object
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.ceil(4.1));
+// console.log(Math.min(4,6,8,99,3,-3));
+// console.log(Math.max(4,6,8,99,3,-3));
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1); // random number between 1 to 10
+console.log(Math.floor(Math.random() * 100) + 1); // random number between 1 to 100
+console.log(Math.floor(Math.random() * 50) + 50); // random number between 50 to 99
+
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.sqrt(64));
+// console.log(4 ** (1/2));
+// console.log(Math.cbrt(27));
+// console.log(27 ** (1/3));
+// console.log(Math.log(2));
+// console.log(Math.log2(8));
+// console.log(Math.log10(1000));
+// console.log(Math.sin(0));
+// console.log(Math.cos(0));
+// console.log(Math.tan(0));
+
+ const min = 10
+ const max = 20
+
+console.log(Math.floor(Math.random()* (max - min + 1)) + min); // random number between min to max
+
+
+
+
+
+
+
