29aa03f2326c56ed1db9f0819239b2335b76c3be
Rajrup Das <166900923+coder-rajrup@users.noreply.github.com>
2025-10-11 12:57:26 +0000

    function

--- 01_basics/10_objects.js
+++ 01_basics/10_objects.js
@@ -1,5 +1,7 @@
 //singleton
 
+//Part 1
+
 //object literal 
 
 const mySym = Symbol("key1");
@@ -16,13 +18,96 @@ const JsUser = {
 
 // console.log(JsUser);
 // console.log(JsUser["email"]);
-console.log(JsUser[mySym]);
+// console.log(JsUser[mySym]);
 
 
 
 
 
-// JsUser.email = "raj@chatgpt.com";
-// Object.freeze(JsUser);
-// JsUser.email = "raj@gmail.com";
+JsUser.email = "raj@chatgpt.com";
+Object.freeze(JsUser);
+JsUser.email = "raj@gmail.com";
 // console.log(JsUser.email);
+
+JsUser.greeting = function() {
+    // console.log("Hello JsUser");
+}
+
+// console.log(JsUser.greeting());
+
+
+JsUser.greetingTwo = function() {
+    // console.log(`Hello JsUser ${this.name}`);
+}
+
+// console.log(JsUser.greetingTwo());
+
+
+//++++++++++++++ Part 2 +++++++++++++
+
+const tinderUser = {}
+
+
+tinderUser.id = "123abc";
+tinderUser.name = "Elon Musk";
+tinderUser.isLoggedIn = false;
+tinderUser.age = 49;
+tinderUser.email = "elon@tinder.com";
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "regular@tinder.com",
+    fullname: {
+        userfullname:{
+            firstname: "Elon",
+            lastname: "Musk" 
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "c", 4: "d"};
+const obj4 = {5: "e", 6: "f"};
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4);
+
+const obj3 = {...obj1, ...obj2, ...obj4};
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: "12345abc",
+        email: "elonmusk@tinder.com"
+    },
+    {
+        id: "456def",
+        email: "musk@tinder.com"
+    }
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+
+
+const course = {
+    coursename: "JavaScript",
+    price: "999",
+    courseInstructor: "Raj",
+    courseLength: "30 days"
+}
+// course.courseInstructor
+
+const{courseInstructor: instructor} = course;
+console.log(instructor);
+
+
+
---
+++ 01_basics/11_function.js
@@ -0,0 +1,53 @@
+function sayMyName(){
+    // console.log("R");
+    // console.log("A");
+    // console.log("J");
+}
+
+// sayMyName();
+
+
+function addTwoNumbers(num1, num2){
+//    let result = num1 + num2;
+//     return result;
+    return num1 + num2; 
+}
+// function addTwoNumbers(num1, num2){
+//     console.log(num1 + num2);
+// }
+
+const result = addTwoNumbers(5, 7)
+// console.log("Result:", result);
+
+
+
+function loginUserMessage(username){
+    return `${username} just logged in`;
+}
+// console.log(loginUserMessage("Rajat"));
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 300, 400, 500)); // 200
+
+const user = {
+    username: "Rajat",
+    price: 500
+}
+
+function handleObject(obj){
+console.log(`Username: ${obj.username}, Price: ${obj.price}`); // Username: Rajat, Price: 500
+    
+}
+ha
+
+
+
+
+
+
+
+
+
