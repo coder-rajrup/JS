9d966b303f13be4cb7e69cb2a3a4e85268b3d4b5
Rajrup Das <166900923+coder-rajrup@users.noreply.github.com>
2025-10-18 16:14:15 +0000

    test

--- 01_basics/14_IIFE.js
+++ 01_basics/14_IIFE.js
@@ -1,7 +1,7 @@
 // Immediately Invoked Function Expression (IIFE)
 
     (function chai(){
-        console.log(" DB CONNECTED ");
+        // console.log(" DB CONNECTED ");
     })(); // Normal Function Call
 
 
@@ -11,9 +11,23 @@
 //     }   )(); // Arrow Function Call
 
 
-(   (name) => {
-        console.log(`DB CONNECTED ARROW ${name}`);
-    }   )("RAJ"); // Arrow Function Call
+// (   (name) => {
+        // console.log(`DB CONNECTED ARROW ${name}`);
+    // }   )("RAJ"); // Arrow Function Call
+
+
+let val1 = 10
+let val2 = 20
+
+function addNum(num1, num2){
+    let total = num1 + num2
+     return total
+}
+let result1 = addNum(val1, val2)
+let result2 = addNum(100, 200)
+
+console.log(result1);
+console.log(result2);
 
 
 
