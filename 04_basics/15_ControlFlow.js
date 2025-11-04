d822285581abc4d9e4110327d9d1e0aa9e603a98
Rajrup Das <dasrajrup4@gmail.com>
2025-11-05 00:04:46 +0530

    if else & true false

--- 04_basics/15_ControlFlow.js
+++ 04_basics/15_ControlFlow.js
@@ -2,18 +2,97 @@
     
 // }
 
-const isUserLoggedIn = true;
+// const isUserLoggedIn = true;
+// const temperature = 25;
 
-if (isUserLoggedIn) {
-    console.log("User is logged in");
-} else {
-    console.log("User is not logged in");
-}
+// if (isUserLoggedIn) {
+//     console.log("User is logged in");
+// } else {
+//     console.log("User is not logged in");
+// }
 
 
-if( 2 == "2"){
-    console.log("Executed");
-}
+// if( 2 == "2"){
+//     console.log("Executed");
+// }
+
+// const isUserLoggedIn = true
+// const debitCard = true;
+// const loggedInFromGoogle = false;
+// const loggedInFromFacebook = false;
+// const loggedInFromEmail = false;
+
+// if(isUserLoggedIn && debitCard && 2==2){
+    // console.log("You can make the purchase");
+// } else {
+    // console.log("You cannot make the purchase");
+// }
+
+// if(loggedInFromGoogle || loggedInFromFacebook || loggedInFromEmail){
+//     console.log("User is logged in");
+// } else {
+//     console.log("User is not logged in");
+// }
+    const userEmail = [];
+
+
+    if(userEmail)
+    {       
+    //     console.log("Got the email");
+    }
+    else{
+    //     console.log("Email not found");
+    }
+
+     if(userEmail.length === 0){
+    //     console.log("Array is empty");
+     }  
+     else{ 
+    //     console.log("Array is not empty");
+        
+     }
+
+
+// const emptyObj = {};
+
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("Object is empty");
+// } else {
+ 
+//     console.log("Object is not empty");
+// }
+
+
+// Nullish Coalescing Operator (??) : null or undefined
+let val1;
+// val1 = 5 ?? 10; 
+val1 = null ?? 10;
+// console.log(val1); // Output: 10
+
+val1 = undefined ?? 10;
+// console.log(val1); // Output: 10
+
+
+val1 = null ?? undefined ?? 20;
+// console.log(val1); // Output: 20
+
+val1 = null ?? 0 ?? 25;
+// console.log(val1); // Output: 0
+
+
+// Ternary Operator
+
+ const age = 18;
+ age >= 18 ? console.log("You are eligible to vote") : console.log("You are not eligible to vote");
+
+
+
+
+
+
+
+
+
 
 
 
