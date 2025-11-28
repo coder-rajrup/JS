878a359092adba41000edf0e4737b427ff3936ba
Rajrup Das <dasrajrup4@gmail.com>
2025-11-28 23:08:30 +0530

    continue@break

--- 05_basics/01_ForLoop.js
+++ 05_basics/01_ForLoop.js
@@ -10,14 +10,44 @@
 // }
 
 for(let i = 5; i >= 0; i--){
-  console.log(`Outer loop: ${i}`);
+  // console.log(`Outer loop: ${i}`);
 
  for (let j = 0; j <= i; j++){
     // console.log(` Inner loop: ${j} and innner loop ${i}`);
-    console.log(i + '*' + j + ' = ' + i*j );
+    // console.log(i + '*' + j + ' = ' + i*j );
     
   }
 
 }
+let myArray = ['Japan', 'Paris', 'China', 'Rome', 'Germany'];
+// console.log('---- Array Elements ----');
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  // console.log(element);
+  
+}
+
+
+//break and continue
+for (let i = 0; i <= 10; i++) {
+  if(i == 5){
+    // break;
+    console.log(`Detected 5`);
+    
+    continue;
+  }
+  console.log(`Value of i is ${i}`);
+  
+}
+
+
+
+
+
+
+
+
+
+
 
 
