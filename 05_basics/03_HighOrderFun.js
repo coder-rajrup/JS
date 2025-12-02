f5ec529d4721b8d2a2446bf3669d8a161c00c008
Rajrup Das <dasrajrup4@gmail.com>
2025-12-02 21:44:42 +0530

    ForEach Loops

---
+++ 05_basics/03_HighOrderFun.js
@@ -0,0 +1,42 @@
+const coding = [ "JavaScript", "Python", "C++", "Java" ];
+
+// coding.forEach( function (val) {
+//     console.log(val);
+// } ) 
+    
+
+coding.forEach( (item) => {
+    // console.log(item); 
+})
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe);
+
+
+
+// coding.forEach( (item, index, arr)=>{
+//     console.log(item, index, arr);
+// })
+
+
+const fruits = [
+  { name: "apple", color: "red" },
+  { name: "banana", color: "yellow" },
+  { name: "cherry", color: "dark red" },
+];
+ 
+fruits.forEach( (item)=> {
+     
+    console.log(item);
+    
+})
+
+
+
+
+
+
